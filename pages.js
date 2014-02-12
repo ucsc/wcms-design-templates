d1a5a6920b002ead059637635db6cfd786c36c69
Rob Knight <raknight@ucsc.edu>
2014-02-12 15:04:11 -0800

    Add more page list examples.

--- pages.js
+++ pages.js
@@ -46,11 +46,47 @@ exports.pages=[
 		"site": "http://history.ucsc.edu",
 		"path": "/",
 		"file": "index.html"
-	},
+	},	
 	{		
 		"name": "simplified-home",
 		"site": "http://audit.ucsc.edu",
 		"path": "/",
 		"file": "index.html"
+	},
+	{		
+		"name": "google-calendar-page",
+		"site": "http://history.ucsc.edu",
+		"path": "/news-events/events/",
+		"file": "index.html"
+	},
+	{		
+		"name": "featured-profile",
+		"site": "http://history.ucsc.edu",
+		"path": "/news-events/profiles/",
+		"file": "lia-winfield.html"
+	},
+	{		
+		"name": "course-catalog",
+		"site": "http://history.ucsc.edu",
+		"path": "/courses/",
+		"file": "class-schedule-view.php"
+	},
+	{		
+		"name": "course-schedule",
+		"site": "http://history.ucsc.edu",
+		"path": "/courses/",
+		"file": "class-schedule-view.php?detail=y&strm=2140&class_nbr=42298"
+	},	
+	{		
+		"name": "directory-list",
+		"site": "http://history.ucsc.edu",
+		"path": "/about/",
+		"file": "staff.php"
+	},
+	{		
+		"name": "directory-item",
+		"site": "http://history.ucsc.edu",
+		"path": "/about/",
+		"file": "singleton.php?&singleton=true&cruz_id=sshinkle"
 	}
 ]
