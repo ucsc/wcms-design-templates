87f281f0d4ea076fb4b1812a6e97ce007d321d4c
Rob Knight <raknight@ucsc.edu>
2014-06-03 14:13:32 -0700

    Use Assemble to build same pages.
    This includes breaking out page regions into more modular "partials" for better maintainability. This is kind of meta-work, but it will enable us to use these sample pages as a style guide later on.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,16 +1,19 @@
 var pkg = require('./package.json'),
     gulp = require('gulp'),
+    changed = require('gulp-changed'),
     autoprefix = require('gulp-autoprefixer'),
     compass = require('gulp-compass'),
     concat = require('gulp-concat'),
     minifycss	= require('gulp-minify-css'),
     clean = require('gulp-clean'),
-    notify = require('gulp-notify'),
     uglify = require('gulp-uglify'),
     refresh = require('gulp-livereload'),
-    fileinclude = require('gulp-file-include'),
+    assemble = require('gulp-assemble'),
     prettify = require('gulp-html-prettify'),
+    replace = require('gulp-replace'),
     imagemin = require('gulp-imagemin'),
+    svgmin = require('gulp-svgmin'),
+    moment = require('moment'),
     gulpBowerFiles = require('gulp-bower-files'),
     connect = require('gulp-connect');
 
@@ -23,16 +26,19 @@ gulp.task('webserver', function() {
   });
 });
 
-// Clean the build folder so we start fresh
+
+// Clean the build folder so we start clean
 gulp.task('clean', function() {
   gulp.src(['app/build/css', 'app/build/js', 'app/build/*.html'], {read: false})
   .pipe(clean());
 });
 
-// Run Sass with Compass to compile, prefix, and compress styles
-// then copy to the build folder.
+
+// Run Sass with Compass to compile, prefix, 
+// and compress styles then copy to the build folder.
 gulp.task('styles', function() {
   gulp.src(['./app/sass/*.scss'])
+  .pipe(changed('./app/build/css/'))
   .pipe(compass({
    css: './app/build/css',
    sass: './app/sass',
@@ -46,6 +52,7 @@ gulp.task('styles', function() {
   .pipe(connect.reload());
   });
 
+
 // Uglify scripts into the build folder.
 gulp.task('scripts', function() {
  return gulp.src('./app/js/**/**')
@@ -54,40 +61,58 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest('./app/build/js/'));
 });
 
+
 // Optimize and copy images into the build folder.
 gulp.task('images', function() {
  return gulp.src('./app/images/**/**')
-    // Pass in options to the task
+    .pipe(changed('./app/build/images/'))
+    .pipe(svgmin())
     .pipe(imagemin({optimizationLevel: 5}))
     .pipe(gulp.dest('./app/build/images/'));
 });
 
-// Add partials to HTML, then prettify and copy into the build folder.
-gulp.task('templates', function() {
-  gulp.src(['./app/*.html'])
-  .pipe(fileinclude())
-  .pipe(prettify({indent_char: ' ', indent_size: 2}))
-  .pipe(gulp.dest('./app/build/'))
-  .pipe(connect.reload());
+
+// Assemble handlebars page templates and prettify the HTML.
+gulp.task('assemble', function () {
+
+  var compiled = moment().format('MMMM DD, YYYY - HH:mm:ss');
+  
+  var assembleOptions = {
+    data: './app/data/*.json',
+    partials: './app/partials/*.hbs',
+    layoutdir: './app/layouts/'
+  };
+
+  gulp.src('./app/pages/*.hbs')
+    .pipe(changed('./app/build/'))
+    .pipe(assemble(assembleOptions))
+    .pipe(prettify({indent_char: ' ', indent_size: 2}))
+    .pipe(replace(/##DATE##/g, compiled))
+    .pipe(gulp.dest('./app/build/'))
+    .pipe(connect.reload());
 });
 
-// Add partials to HTML, then prettify and copy into the build folder.
+
+// Copy font files into the build folder.
 gulp.task('fonts', function() {
   gulp.src('./app/fonts/*')
   .pipe(gulp.dest('./app/build/fonts/'));
 });
+
 
 // Copy Bower assets into the build folder.
 gulp.task('bower-files', function(){
     gulpBowerFiles().pipe(gulp.dest("./app/build/lib"));
 });
 
+
 // Watchers
 gulp.task('watch', function () {
+  gulp.watch('app/js/**/**', ['scripts']);
   gulp.watch('app/sass/**/*.scss', ['styles']);
   gulp.watch('app/images/*.*', ['images']);
-  gulp.watch(['app/*.html', 'app/html-partials/*.html'], ['templates']);
+  gulp.watch(['app/layouts/*.hbs', 'app/partials/*.hbs', 'app/pages/*.hbs'], ['assemble']);
 });
 
 // The default task (called when you run `gulp`)
-gulp.task('default', ['clean', 'bower-files', 'styles', 'scripts', 'images', 'fonts', 'templates', 'webserver', 'watch']);
+gulp.task('default', ['clean', 'bower-files', 'styles', 'scripts', 'images', 'fonts', 'assemble', 'webserver', 'watch']);
