cb0a8f9ee14dfa0196a210e98088f9b8d65b66b8
Rob Knight <raknight@ucsc.edu>
2015-04-06 13:06:24 -0700

    Gulp will manage and compile styles

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,24 +3,23 @@
 //
 var pkg = require('./package.json'),
     gulp = require('gulp'),
-    assemble = require('assemble'),
-    gulpAssemble = require('gulp-assemble'),
-    browserSync = require('browser-sync'),
-    changed = require('gulp-changed'),
     autoprefix = require('gulp-autoprefixer'),
-    sass = require('gulp-ruby-sass'),
+    changed = require('gulp-changed'),
     concat = require('gulp-concat'),
-    minifycss = require('gulp-minify-css'),
-    clean = require('gulp-clean'),
-    uglify = require('gulp-uglify'),
     prettify = require('gulp-html-prettify'),
-    replace = require('gulp-replace'),
+    imacss = require('gulp-imacss'),
     imagemin = require('gulp-imagemin'),
-    moment = require('moment'),
+    minifycss = require('gulp-minify-css'),
+    replace = require('gulp-replace'),
+    sass = require('gulp-ruby-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    uglify = require('gulp-uglify'),
+    zip = require('gulp-zip'),
+    browserSync = require('browser-sync'),
+    del = require('del'),
     mainBowerFiles = require('main-bower-files'),
     svgo = require('imagemin-svgo'),
-    imacss = require('gulp-imacss'),
-    zip = require('gulp-zip');
+    moment = require('moment');
 
 
 
@@ -28,10 +27,10 @@ var pkg = require('./package.json'),
 // Set default file path variables for tasks
 //
 var paths = {
-    styles:     ['./src/sass/**.scss', './src/sass/**/**.**'],
-    scripts:    './src/js/**/**',
-    images:     './src/images/**/**',
-    svg:        './src/svg/**/**.svg'
+    styles:     './src/assets/sass',
+    scripts:    './src/assets/js/**/**',
+    images:     './src/assets/images/**/**',
+    svg:        './src/assets/svg/**/**.svg'
 };
 
 
@@ -51,33 +50,33 @@ gulp.task('webserver', function() {
 
 
 //
-// Clean the build folder so we start clean
+// Clean the build folder.
 //
-gulp.task('clean', function() {
-    gulp.src([
-        'build/images',
-        'build/css',
-        'build/js'
-        ], {
-        read: false
-    })
-    .pipe(clean());
+gulp.task('clean', function(cb) {
+    del([
+        // 'build/images',
+        'build/assets/css/**',
+        // 'build/js'
+    ], cb);
 });
 
 
 //
-// Compile sass into CSS without source map.
+// Compile sass into CSS and a source map.
 //
 gulp.task('styles', function() {
-    return sass('src/sass', {
-        require: ['bourbon', 'neat']
+    return sass(paths.styles, {
+        require: ['bourbon', 'neat'],
+        style: 'compressed',
+        sourcemap: true
     }) 
     .on('error', function (err) {
       console.error('Error!', err.message);
    })
     .pipe(autoprefix('last 4 versions'))
-    .pipe(minifycss())
-    .pipe(gulp.dest('build/css'));
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('build/assets/css'));
 });
 
 
@@ -98,11 +97,11 @@ gulp.task('scripts', function() {
 //
 gulp.task('images', function() {
     return gulp.src(paths.images)
-        .pipe(changed('./build/images/'))
+        .pipe(changed('./build/assets/images/'))
         .pipe(imagemin({
             optimizationLevel: 5
         }))
-        .pipe(gulp.dest('./build/images/'));
+        .pipe(gulp.dest('./build/assets/images/'));
 });
 
 
@@ -113,7 +112,7 @@ gulp.task('svg', function() {
     return gulp.src(paths.svg)
         .pipe(svgo()())
         .pipe(imacss('_svg.scss', 'icon'))
-        .pipe(gulp.dest('./src/sass/base'));
+        .pipe(gulp.dest('./src/assets/sass/base'));
 });
 
 
@@ -124,19 +123,16 @@ gulp.task('bower-files', function() {
     return gulp.src(mainBowerFiles(), {
             base: './bower_components'
         })
-        .pipe(gulp.dest("./build/lib"));
+        .pipe(gulp.dest("./build/assets/lib"));
 });
 
 
 //
-// Create zip archive of static file assets
+// Create zip archive of static file assets ready for the WCMS.
 //
 gulp.task('deploy', function() {
     return gulp.src([
-            './build/assets/css/**.**',
-            './build/assets/images/**/**.**',
-            './build/assets/js/**.**',
-            './build/assets/lib/**/**.**'
+            './build/assets/**'
         ], {
             base: "./build/assets"
         })
@@ -148,18 +144,18 @@ gulp.task('deploy', function() {
 //
 // The default task (called when you run `gulp`)
 //
-gulp.task('default', ['bower-files', 'styles', 'scripts', 'images', 'svg', 'webserver'], function () {
-    gulp.watch('src/js/**/**', ['scripts']);
-    gulp.watch('src/sass/**/*.scss', ['styles']);
-    gulp.watch('src/images/**/.**', ['images']);
-    gulp.watch('src/svg/**/**.svg', ['svg']);
+gulp.task('default', ['clean', 'bower-files', 'scripts', 'images', 'svg', 'styles', 'webserver'], function () {
+    gulp.watch('src/assets/js/**/**', ['scripts']);
+    gulp.watch('src/assets/sass/**/*.scss', ['styles']);
+    gulp.watch('src/assets/images/**/.**', ['images']);
+    gulp.watch('src/assets/svg/**/**.svg', ['svg']);
 });
 
 
 //
 // The fresh task: compiles everything so we can zip it up for Cascade with 'gulp build'.
 //
-gulp.task('fresh', ['clean', 'bower-files', 'styles', 'scripts', 'images']);
+gulp.task('fresh', ['clean', 'bower-files', 'scripts', 'images', 'svg', 'styles']);
 
 
 
