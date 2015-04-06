a1cf3524a54630b9601fb0a17feb713fe45f6e9a
Rob Knight <raknight@ucsc.edu>
2015-04-06 13:07:17 -0700

    Remove deploy from repo.

--- deploy/_responsive/js/fancybox.init.js
+++
@@ -1,1 +0,0 @@
-jQuery(document).ready(function(){jQuery("#video a.fb").each(function(){jQuery(this).fancybox({titleShow:!1,overlayOpacity:"0.8",href:this.href.replace(new RegExp("watch\\?v=","i"),"v/"),type:"swf",swf:{wmode:"transparent",allowfullscreen:"true"}})}),jQuery("#view a").fancybox(),jQuery(".thumb-grid a").fancybox()});
