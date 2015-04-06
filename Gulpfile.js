a8a17b091f5d7b10b4f828f18d1c11073d54f3fb
Rob Knight <raknight@ucsc.edu>
2015-04-06 16:58:40 -0700

    Dumptruck for backup purposes.

--- Gulpfile.js
+++ Gulpfile.js
@@ -30,7 +30,7 @@ var paths = {
     styles:     './src/assets/sass',
     scripts:    './src/assets/js/**/**',
     images:     './src/assets/images/**/**',
-    svg:        './src/assets/svg/**/**.svg'
+    svg:        './src/assets/images/svg/**/**.svg'
 };
 
 
@@ -40,11 +40,13 @@ var paths = {
 gulp.task('webserver', function() {
     browserSync.init("./build/index.html", {
         server: {
-            baseDir: "./build"            
+            baseDir: "./build",                      
         },
         watchOptions: {
             debounceDelay: 3000
-        }
+        },
+        open: false,
+        port: 8080
     });
 });
 
@@ -54,9 +56,9 @@ gulp.task('webserver', function() {
 //
 gulp.task('clean', function(cb) {
     del([
-        // 'build/images',
-        'build/assets/css/**',
-        // 'build/js'
+        'build/_responsive/images/**',
+        'build/_responsive/css/**',
+        'build/_responsive/js/**'
     ], cb);
 });
 
@@ -76,7 +78,7 @@ gulp.task('styles', function() {
     .pipe(autoprefix('last 4 versions'))
     .pipe(sourcemaps.init())
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('build/assets/css'));
+    .pipe(gulp.dest('build/_responsive/css'));
 });
 
 
@@ -86,9 +88,9 @@ gulp.task('styles', function() {
 gulp.task('scripts', function() {
     return gulp.src(paths.scripts)
         // Pass in options to the task
-        .pipe(changed('./build/js/'))
+        .pipe(changed('./build/_responsive/js/'))
         .pipe(uglify())
-        .pipe(gulp.dest('./build/js/'));
+        .pipe(gulp.dest('./build/_responsive/js/'));
 });
 
 
@@ -97,11 +99,11 @@ gulp.task('scripts', function() {
 //
 gulp.task('images', function() {
     return gulp.src(paths.images)
-        .pipe(changed('./build/assets/images/'))
+        .pipe(changed('./build/_responsive/images/'))
         .pipe(imagemin({
             optimizationLevel: 5
         }))
-        .pipe(gulp.dest('./build/assets/images/'));
+        .pipe(gulp.dest('./build/_responsive/images/'));
 });
 
 
@@ -123,7 +125,7 @@ gulp.task('bower-files', function() {
     return gulp.src(mainBowerFiles(), {
             base: './bower_components'
         })
-        .pipe(gulp.dest("./build/assets/lib"));
+        .pipe(gulp.dest("./build/_responsive/lib"));
 });
 
 
@@ -132,9 +134,9 @@ gulp.task('bower-files', function() {
 //
 gulp.task('deploy', function() {
     return gulp.src([
-            './build/assets/**'
+            './build/_responsive/**'
         ], {
-            base: "./build/assets"
+            base: "./build/_responsive"
         })
         .pipe(zip('_responsive.zip'))
         .pipe(gulp.dest('./deploy'));
@@ -145,10 +147,10 @@ gulp.task('deploy', function() {
 // The default task (called when you run `gulp`)
 //
 gulp.task('default', ['clean', 'bower-files', 'scripts', 'images', 'svg', 'styles', 'webserver'], function () {
-    gulp.watch('src/assets/js/**/**', ['scripts']);
-    gulp.watch('src/assets/sass/**/*.scss', ['styles']);
-    gulp.watch('src/assets/images/**/.**', ['images']);
-    gulp.watch('src/assets/svg/**/**.svg', ['svg']);
+    gulp.watch('./src/assets/js/**/**', ['scripts']);
+    gulp.watch('./src/assets/sass/**/*.scss', ['styles']);
+    gulp.watch('./src/assets/images/**/.**', ['images']);
+    gulp.watch('./src/assets/svg/**/**.svg', ['svg']);
 });
 
 
