202feb03432aa8780764fb03c6ba1ea64ec4ce14
Rob Knight <raknight@ucsc.edu>
2015-05-21 16:59:09 -0700

    Increase max-width of profile and article images. Deals with #308
    When the page finishes loading, this script correctly sizes the images
    in news articles and profiles by looking at their natural size and
    adding 16px (to account for padding) and setting the width of the
    figure element to that number.

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,11 @@
 /*
 // DROPDOWN MENU
 function dropDownMenu() {
-  
+
   var dropdownLinks = $(".dropdown-link");
   var dropdownWrapper = $(this).next(".dropdown-wrapper");
   var dropdownSpacer = $("#dropdown-spacer");
-  
+
   //if all dropdown menus are closed
   if ( !($(dropdownLinks).hasClass("selected")) ) {
     $(this).addClass("selected");
@@ -13,31 +13,31 @@ function dropDownMenu() {
       height: "115"
     }, "fast");
     $(dropdownWrapper).slideDown("fast");
-  } 
+  }
   //if $(this) dropdown menu is open
-  else if ( $(this).hasClass("selected") ) { 
+  else if ( $(this).hasClass("selected") ) {
     $(this).removeClass("selected");
     $(dropdownSpacer).animate({
       height: "0"
     }, "fast");
     $(dropdownWrapper).slideUp("fast");
-  } 
+  }
   //if another dropdown menu is open
-  else if ( $(dropdownLinks).not(this).hasClass("selected") ) { 
+  else if ( $(dropdownLinks).not(this).hasClass("selected") ) {
     $(dropdownLinks).removeClass("selected");
     $(this).addClass("selected");
     $(".dropdown-wrapper").hide();
     $(dropdownWrapper).show();
   }
-  
+
   //don't follow the link
   return false;
 }
 */
 
 $(document).ready(function(){
-  
-  // "Enable" javascript by adding a body class. 
+
+  // "Enable" javascript by adding a body class.
   $('body').addClass('js');
 
   // Remove default value from search input on focus.
@@ -81,51 +81,54 @@ $('.sidebar li > ul').each(function() {
 
   $(".close").click(function(){ //close menu
     $('.selected').focus();
-    $(".dropdown-link").removeClass('selected');    
+    $(".dropdown-link").removeClass('selected');
     $(this).parents(".dropdown-wrapper").slideUp("fast");
     $("#dropdown-spacer").animate({
         height: ""
     }, "fast");
-  
+
     return false;
   });
   */
-  
+
   $('ul#mainNav li').click(function(e){
     $(this).children('ul').slideToggle();
     e.stopPropagation();
   });
-  
-  
+
+
 });
 
 $(window).load(function() {
 
 /**
  * UI-tweak: This looks at the natural width of article/profile
- * images and sets a class on the parent <figure> element to
- * establish the maximum width of the figure, which prevents
- * the image from stretching too much.
+ * images and sets the width of the parent <figure> element to
+ * x+16 to match the width of the image.
  *
  * Called with the window.load event to ensure
  * all images have loaded.
  */
-$(".article-image img").each(function() { 
-  if(this.naturalWidth > 580) {
-    $(this).parent().addClass("width-full");
-  } else if(this.naturalWidth > 480) {
-      $(this).parent().addClass("width-580");
-  } else if(this.naturalWidth > 350) {
-      $(this).parent().addClass("width-480");
-  } else if(this.naturalWidth > 280) {
-      $(this).parent().addClass("width-350");
-  } else if(this.naturalWidth > 180) {
-      $(this).parent().addClass("width-280");
-  } else if(this.naturalWidth > 110) {
-      $(this).parent().addClass("width-180");
-  } else {
-      $(this).parent().addClass("width-110");
-  }
+$(".article-image img").each(function() {
+
+  var newWidth = this.naturalWidth + 16;
+  $(this).parent().css({"width": newWidth});
+
+  // if(this.naturalWidth > 580) {
+  //   $(this).parent().addClass("width-full");
+  // } else if(this.naturalWidth > 480) {
+  //     $(this).parent().addClass("width-580");
+  // } else if(this.naturalWidth > 350) {
+  //     $(this).parent().addClass("width-480");
+  // } else if(this.naturalWidth > 280) {
+  //     $(this).parent().addClass("width-350");
+  // } else if(this.naturalWidth > 180) {
+  //     $(this).parent().addClass("width-280");
+  // } else if(this.naturalWidth > 110) {
+  //     $(this).parent().addClass("width-180");
+  // } else {
+  //     $(this).parent().addClass("width-110");
+  // }
 });
 
 
@@ -140,6 +143,3 @@ $(".article-image img").each(function() {
  * Licensed under the MIT license: http://opensource.org/licenses/MIT
  *
 */jQuery&&function(e){function t(t,i){var s=t?e(this):i,o=e(s.attr("data-dropdown")),u=s.hasClass("dropdown-open");if(t){if(e(t.target).hasClass("dropdown-ignore"))return;t.preventDefault();t.stopPropagation()}else if(s!==i.target&&e(i.target).hasClass("dropdown-ignore"))return;n();if(u||s.hasClass("dropdown-disabled"))return;s.addClass("dropdown-open");o.data("dropdown-trigger",s).show();r();o.trigger("show",{dropdown:o,trigger:s})}function n(t){var n=t?e(t.target).parents().addBack():null;if(n&&n.is(".dropdown")){if(!n.is(".dropdown-menu"))return;if(!n.is("A"))return}e(document).find(".dropdown:visible").each(function(){var t=e(this);t.hide().removeData("dropdown-trigger").trigger("hide",{dropdown:t})});e(document).find(".dropdown-open").removeClass("dropdown-open")}function r(){var t=e(".dropdown:visible").eq(0),n=t.data("dropdown-trigger"),r=n?parseInt(n.attr("data-horizontal-offset")||0,10):null,i=n?parseInt(n.attr("data-vertical-offset")||0,10):null;if(t.length===0||!n)return;t.hasClass("dropdown-relative")?t.css({left:t.hasClass("dropdown-anchor-right")?n.position().left-(t.outerWidth(!0)-n.outerWidth(!0))-parseInt(n.css("margin-right"),10)+r:n.position().left+parseInt(n.css("margin-left"),10)+r,top:n.position().top+n.outerHeight(!0)-parseInt(n.css("margin-top"),10)+i}):t.css({left:t.hasClass("dropdown-anchor-right")?n.offset().left-(t.outerWidth()-n.outerWidth())+r:n.offset().left+r,top:n.offset().top+n.outerHeight()+i})}e.extend(e.fn,{dropdown:function(r,i){switch(r){case"show":t(null,e(this));return e(this);case"hide":n();return e(this);case"attach":return e(this).attr("data-dropdown",i);case"detach":n();return e(this).removeAttr("data-dropdown");case"disable":return e(this).addClass("dropdown-disabled");case"enable":n();return e(this).removeClass("dropdown-disabled")}}});e(document).on("click.dropdown","[data-dropdown]",t);e(document).on("click.dropdown",n);e(window).on("resize",r)}(jQuery);
-
-
-
