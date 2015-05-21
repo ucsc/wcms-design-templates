d0057a8f0540648780fa44bf5a4ad475f67cb98c
Rob Knight <raknight@ucsc.edu>
2015-05-21 16:56:30 -0700

    Update to universal analytics and adapt event tracking.

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -1,24 +1,21 @@
-// Set account and track pageviews
-var _gaq = _gaq || [];
-_gaq.push(['b._setAccount', 'UA-4301164-1']);
-_gaq.push(['b._setDomainName', 'ucsc.edu']);
-_gaq.push(['b._trackPageview']);
+// Universal Analytics snippet - 21 May 2015
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-// Call asynchronous tracking script
-(function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+ga('create', 'UA-4301164-1', 'auto', {'autoLinker': true});
+ga('require', 'linker');
+ga('linker:autoLink', ['imodules.com']);
+ga('send', 'pageview');
 
-// Event tracking calls
-// jQuery required for DOM selection
+
+// Event tracking calls (jQuery required for DOM selection)
 $(function() {
 
-	
 	// Default link tracking
-	$('a[href]').each(function() {				
-		
+	$('a[href]').each(function() {
+
 		var currPageTitle = document.title; // Page title
 		var currPagePath = window.location.pathname; // URL path
 		var linkURL = $(this).attr('href'); // Hyperlink URL
@@ -28,14 +25,14 @@ $(function() {
 		if ( linkURL.match(/^https?\:/i) && (!linkURL.match(document.domain)) ) {
 			$(this).click(function() {
 				var outboundURL = linkURL.replace(/^https?\:\/\//i, ''); // Outbound URL without http(s)
-				_gaq.push(['_trackEvent', currPageTitle + ' - (' + currPagePath + ')', 'Click', linkText + ' (' + outboundURL + ')']);
+				ga('send', 'event', 'Link', 'Click', linkText + ' (' + outboundURL + ')', {'nonInteraction': 1});
 			});
 		}
 		// Email link
 		if ( linkURL.match(/^mailto\:/i) ) {
 			$(this).click(function() {
 				var emailAddress = linkURL.replace(/^mailto\:/i, ''); // Email link without mailto:
-				_gaq.push(['_trackEvent', currPageTitle + ' - (' + currPagePath + ')', 'Click - email', linkText + ' (' + emailAddress + ')']);
+				ga('send', 'event', 'Link', 'Click - email', linkText + ' (' + emailAddress + ')', {'nonInteraction': 1});
 			});
 		}
 		// Download link
@@ -43,11 +40,11 @@ $(function() {
 			$(this).click(function() {
 				var docExtension = linkURL.slice(linkURL.lastIndexOf(".") +1);
 				var docPath = linkURL.replace(/^https?\:\/\/(www.)ucsc\.edu\//i, ''); // Document file path
-				_gaq.push(['_trackEvent', currPageTitle + ' - (' + currPagePath + ')', 'Click - ' + docExtension, linkText + ' (' + docPath + ')']);
+				ga('send', 'event', 'Link', 'Click - ' + docExtension, linkText + ' (' + docPath + ')', {'nonInteraction': 1});
 			});
 		}
-	
+
 	});
-	
-	
+
+
 });
