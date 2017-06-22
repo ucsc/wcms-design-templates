acd8ecd0241e321349e1338f43c5b57053e376c9
Rob Knight <raknight@ucsc.edu>
2017-06-22 08:59:31 -0700

    Update list of sample pages

--- pages.js
+++ pages.js
@@ -1,116 +1,116 @@
 exports.pages=[
 	{
 		"name": "campus-home",
-		"site": "http://www.ucsc.edu",
+		"site": "https://www.ucsc.edu",
 		"path": "/",
 		"file": "index.html"
 	},
 	{		
 		"name": "campus-landing",
-		"site": "http://www.ucsc.edu",
+		"site": "https://www.ucsc.edu",
 		"path": "/about/",
 		"file": "index.html"
 	},
 	{		
 		"name": "campus-left-column",
-		"site": "http://www.ucsc.edu",
+		"site": "https://www.ucsc.edu",
 		"path": "/research/",
 		"file": "divisions.html"
 	},
 	{		
 		"name": "campus-single-column",
-		"site": "http://www.ucsc.edu",
-		"path": "/mobile/",
-		"file": "mobile_apps.html"
+		"site": "https://www.ucsc.edu",
+		"path": "/help/",
+		"file": "index.html"
 	},
 	{		
 		"name": "campus-news-home",
-		"site": "http://news.ucsc.edu",
+		"site": "https://news.ucsc.edu",
 		"path": "/",
 		"file": "index.html"
 	},
 	{		
 		"name": "campus-news-article",
-		"site": "http://news.ucsc.edu",
+		"site": "https://news.ucsc.edu",
 		"path": "/2014/02/",
 		"file": "friends-foundation-international.html"
 	},	
 	{		
 		"name": "division-home",
-		"site": "http://humanities.ucsc.edu",
+		"site": "https://humanities.ucsc.edu",
 		"path": "/",
 		"file": "index.html"
 	},
 	{		
 		"name": "department-home",
-		"site": "http://history.ucsc.edu",
+		"site": "https://history.ucsc.edu",
 		"path": "/",
 		"file": "index.html"
 	},	
 	{		
 		"name": "simplified-home",
-		"site": "http://audit.ucsc.edu",
+		"site": "https://audit.ucsc.edu",
 		"path": "/",
 		"file": "index.html"
 	},
 	{		
-		"name": "google-calendar-page",
-		"site": "http://history.ucsc.edu",
-		"path": "/news-events/events/",
-		"file": "index.html"
-	},
-	{		
 		"name": "featured-profile",
-		"site": "http://history.ucsc.edu",
+		"site": "https://history.ucsc.edu",
 		"path": "/news-events/profiles/",
 		"file": "lia-winfield.html"
 	},
 	{		
 		"name": "course-catalog",
-		"site": "http://history.ucsc.edu",
+		"site": "https://history.ucsc.edu",
 		"path": "/courses/",
 		"file": "class-schedule-view.php"
 	},
 	{		
 		"name": "course-schedule",
-		"site": "http://history.ucsc.edu",
+		"site": "https://history.ucsc.edu",
 		"path": "/courses/",
 		"file": "class-schedule-view.php?detail=y&strm=2140&class_nbr=42298"
 	},	
 	{		
 		"name": "directory-list",
-		"site": "http://history.ucsc.edu",
+		"site": "https://history.ucsc.edu",
 		"path": "/about/",
-		"file": "staff.php"
+		"file": "faculty.php"
 	},
 	{		
 		"name": "directory-item",
-		"site": "http://history.ucsc.edu",
+		"site": "https://history.ucsc.edu",
 		"path": "/about/",
 		"file": "singleton.php?&singleton=true&cruz_id=sshinkle"
 	},
 	{
 		"name": "department-left-column",
-		"site": "http://www.astro.ucsc.edu",
+		"site": "https://www.astro.ucsc.edu",
 		"path": "/about_department/",
 		"file": "index.html"		
 	},
 	{
 		"name": "department-news-article",
-		"site": "http://www.astro.ucsc.edu",
+		"site": "https://www.astro.ucsc.edu",
 		"path": "/news-events/recent/",
 		"file": "index.html"		
 	},
 	{
 		"name": "department-gallery-page",
-		"site": "http://www.astro.ucsc.edu",
+		"site": "https://www.astro.ucsc.edu",
 		"path": "/about_department/image_gallery/",
 		"file": "gallery-page_people.html"		
 	},
 	{
 		"name": "department-courses-table",
-		"site": "http://history.ucsc.edu",
+		"site": "https://history.ucsc.edu",
 		"path": "/courses/",
 		"file": "courses-at-a-glance.html"		
-	}	
+	},
+	{
+		"name": "feedback-form",
+		"site": "https://www.eeb.ucsc.edu",
+		"path": "/",
+		"file": "feedback.php"
+	}
 ]
