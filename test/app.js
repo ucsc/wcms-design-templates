65ee6f5f03049ea86f605c39b8dab7a18a311d82
Rob Knight <raknight@ucsc.edu>
2022-01-12 14:36:32 -0800

    refactor: 🎨 Remove the last of Fractal and format the preview script file
    We're documenting the design system in a repo called [Truss](https://github.com/ucsc/truss).

--- test/app.js
+++ test/app.js
@@ -1,110 +1,114 @@
 "use strict";
 
-const fs          = require('fs');
-const mkdirp      = require('mkdirp');
-const rp          = require('request-promise');
-const cheerio     = require('cheerio');
-const hbs         = require('handlebars');
-const pages       = require('./pages.js').pages;
-const appPath     = "./dist/";
+const fs = require("fs");
+const mkdirp = require("mkdirp");
+const rp = require("request-promise");
+const cheerio = require("cheerio");
+const hbs = require("handlebars");
+const pages = require("./pages.js").pages;
+const appPath = "./assets/";
 
 // We create a new index file each time we fetch
 // the sample pages, just in case we've added more to the list.
-fs.open(appPath + 'index.html', "r", function (err, fd) {
-    if (fd) {
-        fs.unlink(appPath + 'index.html', function (err) {
-          if (err) throw err;
-          console.log('Removing index.html...');
-        });
-    } 
-    else {
-      console.error('No index.html file. Proceeding...');
-    };
+fs.open(appPath + "index.html", "r", function (err, fd) {
+  if (fd) {
+    fs.unlink(appPath + "index.html", function (err) {
+      if (err) throw err;
+      console.log("Removing index.html...");
+    });
+  } else {
+    console.error("No index.html file. Proceeding...");
+  }
 });
 
-
 pages.forEach(function (i) {
-
   var filename = i.name,
-        site = i.site,
-        path = i.path,
-        file = i.file,
-        url = site + path + file;
+    site = i.site,
+    path = i.path,
+    file = i.file,
+    url = site + path + file;
 
-  // Fetch the content from the live site URL      
+  // Fetch the content from the live site URL
   rp(url)
-    .then(function(html) {
-      
+    .then(function (html) {
       // Load the response into the parser
       var $ = cheerio.load(html);
-      
-      // Iterate over <link> tags to use relative file paths 
-      $('link').map(function(i, el) {
+
+      // Iterate over <link> tags to use relative file paths
+      $("link").map(function (i, el) {
         // Get the 'src' attr for the image and replace with a relative path
-        var hrefSrc = $(this).attr('href').replace(/^(https\:)?\/\/(webassets|static).ucsc.edu\/(_responsive\/)?/gm, "");
-        
-        $(this).attr('href', hrefSrc);
+        var hrefSrc = $(this)
+          .attr("href")
+          .replace(
+            /^(https\:)?\/\/(webassets|static).ucsc.edu\/(_responsive\/)?/gm,
+            ""
+          );
+
+        $(this).attr("href", hrefSrc);
       });
 
-      // Iterate over <script> tags to use relative file paths 
-      $('script').map(function(i, el) {
+      // Iterate over <script> tags to use relative file paths
+      $("script").map(function (i, el) {
         // Get the 'src' attr for the script and replace with a relative path
-        if ($(this).attr('src')) {
-          var scriptSrc = $(this).attr('src').replace(/^(https\:)?\/\/(webassets|static).ucsc.edu\/(_responsive\/)?/gm, "");
-          $(this).attr('src', scriptSrc);
+        if ($(this).attr("src")) {
+          var scriptSrc = $(this)
+            .attr("src")
+            .replace(
+              /^(https\:)?\/\/(webassets|static).ucsc.edu\/(_responsive\/)?/gm,
+              ""
+            );
+          $(this).attr("src", scriptSrc);
         }
       });
 
-      // Iterate over <img> tags to look for relative 'src' paths 
-      $('img').map(function(i, el) {
+      // Iterate over <img> tags to look for relative 'src' paths
+      $("img").map(function (i, el) {
         // Get the 'src' attr for the image
-        var imgsrc = $(this).attr('src');
+        var imgsrc = $(this).attr("src");
         // Crudely see if the path is relative or absolute
-        if (imgsrc.indexOf('http') == -1) {
+        if (imgsrc.indexOf("http") == -1) {
           // Prepend the site path to the 'src' attr
-          $(this).attr('src', site + path + imgsrc);
-        };
+          $(this).attr("src", site + path + imgsrc);
+        }
       });
 
       // Parsed and fixed contents for our local file
       var fileContents = $.html();
 
       // Write the local file
-      mkdirp(appPath, function(err) {
-        fs.writeFile(appPath + filename + ".html", fileContents, function (err) {
-          if (err) throw err;
-          console.log('Fetched ' + url + ' as => ' + filename + '.html');
-        });
+      mkdirp(appPath, function (err) {
+        fs.writeFile(
+          appPath + filename + ".html",
+          fileContents,
+          function (err) {
+            if (err) throw err;
+            console.log("Fetched " + url + " as => " + filename + ".html");
+          }
+        );
       });
-
     })
-    .catch(function(err) {
-      console.log('Error: ' + err);
+    .catch(function (err) {
+      console.log("Error: " + err);
     });
-
 });
 
 // Use Handlebars to convert the list of sample pages
 // to an index file for the local test server
-fs.readFile('test/template.html', function(err, templateFile) {
-
-  hbs.registerHelper('fetched', function() {
+fs.readFile("test/template.html", function (err, templateFile) {
+  hbs.registerHelper("fetched", function () {
     var today = new Date();
     var date = today.toDateString();
-    var time = today.toLocaleTimeString('en-US');
-    return date + ' at ' + time;
+    var time = today.toLocaleTimeString("en-US");
+    return date + " at " + time;
   });
-  
-  var template  = hbs.compile(templateFile.toString());
-  var htmlFinal = template({pages: pages});
-  
-  mkdirp(appPath, function(e) {
-    fs.writeFile(appPath + 'index.html', htmlFinal, (err) => {
+
+  var template = hbs.compile(templateFile.toString());
+  var htmlFinal = template({ pages: pages });
+
+  mkdirp(appPath, function (e) {
+    fs.writeFile(appPath + "index.html", htmlFinal, (err) => {
       if (err) throw err;
-      console.log('index.html file saved');
+      console.log("index.html file saved");
     });
   });
-  
 });
-
-
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,25 +1,30 @@
-let mix = require('laravel-mix');
-
+let mix = require("laravel-mix");
 
 // Compile SASS to CSS
-mix.sass('src/sass/ucsc.scss', 'dist/css', {
-    processUrls: false,
-    sassOptions: {
-        outputStyle: 'compressed'
-    }
+mix.sass("src/sass/ucsc.scss", "assets/css", {
+  processUrls: false,
+  sassOptions: {
+    outputStyle: "compressed",
+  },
 });
 
 // Copy javascript libraries to distribution folder
-mix.copyDirectory('node_modules/owlcarousel/owl-carousel', 'dist/lib/owlcarousel/owl-carousel');
-mix.copyDirectory('node_modules/fancybox/dist', 'dist/lib/fancybox/source');
-mix.copy('node_modules/jquery/dist/jquery.js', 'dist/lib/jquery/dist/jquery.js');
+mix.copyDirectory(
+  "node_modules/owlcarousel/owl-carousel",
+  "assets/lib/owlcarousel/owl-carousel"
+);
+mix.copyDirectory("node_modules/fancybox/dist", "assets/lib/fancybox/source");
+mix.copy(
+  "node_modules/jquery/dist/jquery.js",
+  "assets/lib/jquery/dist/jquery.js"
+);
 
 // Copy images to distribution folder
-mix.copyDirectory('src/js', 'dist/js');
-mix.minify('dist/js/*.js', 'bundle.js');
+mix.copyDirectory("src/js", "assets/js");
+mix.minify("assets/js/*.js", "bundle.js");
 
 // Copy images to distribution folder
-mix.copyDirectory('src/images', 'dist/images');
+mix.copyDirectory("src/images", "assets/images");
 
 // mix.browserSync({
 //     server: "./dist"
